99425be7452dbdd318252d5a78d9fb8fb12a6f95
Team Lana <cedic2005@gmail.com>
2021-08-24 20:51:12 +0200

    calculator.js correction

--- exe/cmd/utility/calculator.js
+++ exe/cmd/utility/calculator.js
@@ -3,7 +3,10 @@ const { evaluate } = require("mathjs")
 module.exports = {
   exe: async function(message, prefix, command, args, lang){
     if (!args[0]) return message.reply(lang.assets.noArgs);
-    message.reply(lang.assets.response.replace(/{calc}/g, evaluate(message.content.slice(prefix.length+command.length+1))))
+    let res = false;
+    try { res = evaluate(message.content.slice(prefix.length+command.length+1)) } catch(err) { res = "ERROR"}
+
+      message.reply(lang.assets.response.replace(/{calc}/g, res))
   },
   config: { name: "calculator", aliases: ["calculate", "calc", "calcul"], category: "utility", system: { perms: ["SEND_MESSAGES"], delInvoke: false, inProgress: false, staffCommand: false, devCommand: false } },
   path: null
