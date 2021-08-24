101048ba40d1b29b10d13b2ee216da043e0a3547
Team Lana <cedic2005@gmail.com>
2021-08-24 20:40:51 +0200

    calculate + avancement profile.js

--- exe/cmd/info/profile.js
+++ exe/cmd/info/profile.js
@@ -15,17 +15,31 @@ exe: async function(message, prefix, command, args, lang){
   // background
   const defaultBackground = await Canvas.loadImage(config.defaultProfileCardBackgroundPath)
   if (!(await database.db.get("user/"+message.author.id))?.profile?.banner){
-    await ctx.drawImage(defaultBackground,0,0);
+    ctx.drawImage(defaultBackground,0,0,1200,675);
   } else {
     const background = await Canvas.loadImage((await database.db.get("user/"+message.author.id))?.profile?.banner)
-    await ctx.drawImage(background,0,0);
+    ctx.drawImage(background,0,0,1200,675);
   }
 
   progress.update(1); await msg.edit(lang.misc.progressMSG.replace(/{bar}/g, progress.draw()));
 
+  // template
+  const template = await Canvas.loadImage(config.profileCardTemplate);
+  ctx.drawImage(template, 0, 0, canvas.width - 10, canvas.height - 10)
+
+  progress.update(1); await msg.edit(lang.misc.progressMSG.replace(/{bar}/g, progress.draw()));
+
+  // pp
+  const pp = await Canvas.loadImage(user.user.displayAvatarURL({ size: 512, format: "png", dynamic: false }));
+  ctx.save();
+  ctx.beginPath();
+  ctx.arc(536, 378, 150, 0, 2 * Math.PI, false);
+  ctx.clip();
+  ctx.drawImage(pp, 536, 378);
+  ctx.restore();
+
   msg.delete().catch(()=>false)
   await message.channel.send({ files: [{name:"profile.png", attachment: canvas.toBuffer()}]})
-  
 },
 config: { name: "profile", aliases: ["p", "rank"], category: "info", system: { perms: ["SEND_MESSAGES", "MANAGE_MESSAGES", "EMBED_LINKS"], delInvoke: false, inProgress: false, staffCommand: false, devCommand: false } },
 path: null
--- exe/cmd/utility/calculator.js
+++ exe/cmd/utility/calculator.js
@@ -1,13 +1,9 @@
-function calculate(str){
-  const calc = (str.split("").filter(e => e.match(/[0-9]|\+|\-|\%|\÷|\×|\x|\/|\*|\(|\)|\[|\]/g)).join("")).replace(/\x|\×/g, "*").replace(/\÷/g, "/");
-  let res = false; try { res = eval(calc) || "ERROR" } catch (err) { res = "ERROR" };
-  return res;
-};
+const { evaluate } = require("mathjs")
 
 module.exports = {
   exe: async function(message, prefix, command, args, lang){
     if (!args[0]) return message.reply(lang.assets.noArgs);
-    message.reply(lang.assets.response.replace(/{calc}/g, calculate(message.content.slice(prefix.length+command.length+1))))
+    message.reply(lang.assets.response.replace(/{calc}/g, evaluate(message.content.slice(prefix.length+command.length+1))))
   },
   config: { name: "calculator", aliases: ["calculate", "calc", "calcul"], category: "utility", system: { perms: ["SEND_MESSAGES"], delInvoke: false, inProgress: false, staffCommand: false, devCommand: false } },
   path: null
